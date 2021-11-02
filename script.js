b0fc002d0fd81c44b03b2b602a3344cbc4e4cb9e
AndrewSimons3 <simdrew07@gmail.com>
2021-11-02 07:53:27 -0500

    fixed

--- script.js
+++ script.js
@@ -52,6 +52,18 @@ btnScrollTo.addEventListener('click', function (e) {
     document.documentElement.clientWidth
   );
 
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
   section1.scrollIntoView({ behavior: 'smooth' });
 });
 
